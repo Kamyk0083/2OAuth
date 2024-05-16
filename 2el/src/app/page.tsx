f3c9114f938c18a5ad2e77f750d5db8a261c0699
Dorszyk11 <116880271+Dorszyk11@users.noreply.github.com>
2024-05-16 03:06:02 +0200

    wysyłanie maili

--- 2el/src/app/page.tsx
+++ 2el/src/app/page.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link';
-import gif from "../200w.gif";
 
 export default function Home() {
+
+
   return (
     <div className="bg-gray-100 h-screen flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold text-gray-800 mb-6">Logowanie dwuetapowe</h1>
@@ -10,9 +11,7 @@ export default function Home() {
         <h1 className="text-2xl font-semibold text-blue-600">Ksawery Kamiński</h1>
       </div>
       <Link href="/login" legacyBehavior>
-        <a className="mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Start
-        </a>
+        <a className="mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Start</a>
       </Link>
     </div>
   );
