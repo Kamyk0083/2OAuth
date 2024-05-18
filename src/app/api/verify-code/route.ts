03f8719906ee8fe47a1683b3fff9d0bb91a343a0
Kamyk0083 <kaminski.ksawery00@gmail.com>
2024-05-18 14:32:51 +0200

    Update route.ts

--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import VerificationCode from "../../../../Verification_code";
 import User from "../../../../User";
 
+export const revalidate = 1;
 export async function POST(req: NextRequest) {
   const { email, code } = await req.json();
   const record = await VerificationCode.findOne({ email, code });
