f59ae57cf112bff1e5609fc031f7fb5499532975
Dorszyk11 <116880271+Dorszyk11@users.noreply.github.com>
2024-05-18 19:12:32 +0200

    poprawki po profesjonalnych badaniach ux

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -29,46 +29,47 @@ export default function Register() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <header className="mb-6" text-gray-700>
-        <h1 className="text-2xl font-bold">
-          Rejerstracja
-        </h1>
-      </header>
-      <main className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
-        <form onSubmit={registerCompany} className="space-y-4">
-          <input
-            type="text"
-            placeholder="Nazwa"
-            className="w-full p-2 border rounded text-black"
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              setCompanyName(e.target.value)
-            }
-          />
-          <input
-            type="text"
-            placeholder="Adres email"
-            className="w-full p-2 border rounded text-black"
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              setCompanyEmail(e.target.value)
-            }
-          />
-          <input
-            type="password"
-            placeholder="Hasło"
-            className="w-full p-2 border rounded text-black"
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              setCompanyPassword(e.target.value)
-            }
-          />
-          <button
-            type="submit"
-            className="w-full py-2 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded"
-          >
-            Zarejestruj
-          </button>
-        </form>
-      </main>
+    <div className="p-8 bg-gray-100">
+      <a href="/login">Powrót</a>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <header className="mb-6" text-gray-700>
+          <h1 className="text-2xl font-bold">Rejestracja</h1>
+        </header>
+        <main className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
+          <form onSubmit={registerCompany} className="space-y-4">
+            <input
+              type="text"
+              placeholder="Nazwa"
+              className="w-full p-2 border rounded text-black"
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setCompanyName(e.target.value)
+              }
+            />
+            <input
+              type="text"
+              placeholder="Adres email"
+              className="w-full p-2 border rounded text-black"
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setCompanyEmail(e.target.value)
+              }
+            />
+            <input
+              type="password"
+              placeholder="Hasło"
+              className="w-full p-2 border rounded text-black"
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setCompanyPassword(e.target.value)
+              }
+            />
+            <button
+              type="submit"
+              className="w-full py-2 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded"
+            >
+              Zarejestruj
+            </button>
+          </form>
+        </main>
+      </div>
     </div>
   );
 }
